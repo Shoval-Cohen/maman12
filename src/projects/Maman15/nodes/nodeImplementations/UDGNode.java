4a2e323159e2d8b6ce0226edc89845063ab06d8e
shoval-cohen <shoval835@gmail.com>
2021-07-14 17:07:20 +0300

    My work with random nodes

---
+++ src/projects/Maman15/nodes/nodeImplementations/UDGNode.java
@@ -0,0 +1,41 @@
+package projects.Maman15.nodes.nodeImplementations;
+
+import sinalgo.configuration.WrongConfigurationException;
+import sinalgo.nodes.Node;
+import sinalgo.nodes.messages.Inbox;
+
+public class UDGNode extends Node {
+
+    public UDGNode() {
+    }
+
+    @Override
+    public void handleMessages(Inbox inbox) {
+
+    }
+
+    @Override
+    public void preStep() {
+
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void neighborhoodChange() {
+
+    }
+
+    @Override
+    public void postStep() {
+
+    }
+
+    @Override
+    public void checkRequirements() throws WrongConfigurationException {
+
+    }
+}
